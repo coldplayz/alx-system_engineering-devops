5b6030ad6aed1795c322b4d3c08c5316864576a3
Greenbel <greenbel.chibuike@gmail.com>
2022-11-11 22:41:58 +0100

    T11-0x05: implementation.

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * infinite_while - loops infinitely.
+ *
+ * Return: 0 always.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+
+	return (0);
+}
 
 /**
  * main - creates 5 zombies.
@@ -7,5 +24,16 @@
  */
 int main(void)
 {
+	pid_t pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		printf("Zombie process created, PID: %d\n", pid);
+	}
+
+	infinite_while();
+
 	return (0);
 }
