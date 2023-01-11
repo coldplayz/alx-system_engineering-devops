9f8e85bf6fd54dd6b8c17d2d5c4b7ecffaefb3fb
greenbel <greenbel.chibuike@gmail.com>
2023-01-11 11:10:52 +0000

    Added conditonal for exec

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Kills a process named `killmenow`
 
 exec { 'killmenow':
-  command => 'usr/bin/env pkill killmenow',
+  command => '/usr/bin/env pkill killmenow',
+  onlyif  => '/usr/bin/env pgrep killmenow',
 }
