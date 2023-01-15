aae3c3b5f068842c9938e9fee67ccac8c990d401
greenbel <greenbel.chibuike@gmail.com>
2023-01-15 14:05:16 +0000

    T3-0x0B: fixed code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,33 @@
 # Sets configuration with Puppet.
+# Ensures IdentityFile and PasswordAuthentication lines in ssh config
 
-file_line { 'use_school':
-  ensure => present,
-  path   => '~/.ssh/config',
-  line   => 'Host *\n  IdentityFile ~/.ssh/school\n',
+# Create config file if not exixts
+exec { 'createFile':
+  command => 'echo "Host *\n    IdentityFile ~/.ssh/id_rsa\n    PasswordAuthentication no" > /etc/ssh/ssh_config',
+  creates => '/etc/ssh/ssh_config',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin',
 }
 
-file_line { 'no_passphrase':
-  ensure  => present,
-  path    => '~/.ssh/config',
-  line    => '  PasswordAuthentication no\n',
-  require => File_line['use_school'],
+# Ensure presence of generic Host configuration block
+exec { 'checkHost':
+  command => 'echo "Host *\n    IdentityFile ~/.ssh/id_rsa\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  unless  => 'grep -x "Host \*" /etc/ssh/ssh_config',
+  require => Exec['createFile'],
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin',
+}
+
+# Ensure presence of desired IdentityFile attribute
+exec { 'idFile':
+  command => 'sed -i "/Host \*/a\    IdentityFile ~\/.ssh\/id_rsa" /etc/ssh/ssh_config',
+  unless  => 'grep -x "    IdentityFile .*ssh.*id_rsa" /etc/ssh/ssh_config',
+  require => Exec['checkHost'],
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin',
+}
+
+# Ensure presence of desired PasswordAuthentication attribute
+exec { 'noPasswd':
+  command => 'sed -i "/Host \*/a\    PasswordAuthentication no" /etc/ssh/ssh_config',
+  unless  => 'grep -x "    PasswordAuthentication no" /etc/ssh/ssh_config',
+  require => Exec['idFile'],
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin',
 }
