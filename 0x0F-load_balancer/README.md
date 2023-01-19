903fabc3fa973b915893bc3df50cb05ec348aaf3
Greenbel <greenbel.chibuike@gmail.com>
2023-01-19 05:35:07 +0100

    T0-0x0F

--- 0x0F-load_balancer/README.md
+++ 0x0F-load_balancer/README.md
@@ -3,4 +3,6 @@ Integrating a load balancer in front of two web servers.
 
 * 0-custom_http_response_header: configures Nginx so that its HTTP response contains a custom header (on web-01 and web-02) - X-Served-By.
 
+* 1-install_load_balancer: installs and configure HAproxy on your lb-01 server.
+
 
--- 0x0F-load_balancer/playground/test.sh
+++ 0x0F-load_balancer/playground/test.sh
@@ -1,42 +1,20 @@
 #!/usr/bin/env bash
-# Install nginx on web server; Nginx should be listening on port 80
-# Querying Nginx at its root (/) with a GET request (requesting a page)
-# 	   curl http://34.198.248.145/   --> returns string "Holberton School"
-#	   curl http://34.198.248.145/   --> returns header info
-# Configures an Nginx server so that /redirect_me is redirecting to another page.
-# Configures an Nginx server to have a custom 404 page that contains the string Ceci n'est pas une page.
-# configures Nginx so that its HTTP response contains a custom header (on web-01 and web-02) - X-Served-By.
+# Installs and configure HAproxy on your lb-01 server.
 
+fend=$(grep -xi "frontend web" hacfg)
+bend=$(grep -xi "backend web-backend" hacfg)
 
-host=$(hostname)
-ERR='\terror_page 404 /404.html;'
-ERR_LOC='\tlocation = /404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}'
-ADD_HDR_XSB='\tadd_header X-Served-By '"$host"
+FRONTEND='frontend web\n\tbind *:80\n\tmode http\n\tdefault_backend web-backend'
+BACKEND='backend web-backend\n\tbalance rounrobin\n\tmode http\n\tserver s1 54.146.64.105:80 check\n\tserver s2 54.90.28.253:80 check'
 
-# See if the redirect location block exists
-location=$(sudo grep "	location = /redirect_me {" cfg)
-# See if error handling is defined
-errdef=$(sudo grep "	error_page 404 /404.html" cfg)
-# Check header
-xsb=$(sudo grep "	add_header X-Served-By" cfg)
-
-if [ -z "$location" ]
+if [ -z "$fend" ]
 then
-
-	# Append a location block for 301 redirection
-	sudo sed -i '/server_name _/a\\n\tlocation = /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}' cfg
-else
-	sudo sed -i 's!https://www.google.com!https://www.youtube.com/watch?v=QH2-TGUlwu4!' cfg
+	# Add frontend configuration
+	echo -e "\n$FRONTEND" >> hacfg
 fi
 
-if [ -z "$errdef" ]
+if [ -z "$bend" ]
 then
-	# Error handling not defined yet
-	sudo sed -i '/server_name _/a\\n'"$ERR"'\n\n'"$ERR_LOC" cfg
-fi
-
-if [ -z "$xsb" ]
-then
-	# No X-Served-By header yet
-	sudo sed -i '/server_name _/a\\n'"$ADD_HDR_XSB" cfg
+	# Add backend configuration
+	echo -e "\n$BACKEND" >> hacfg
 fi
