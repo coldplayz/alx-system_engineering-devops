9661b879827b0e7896bccc7609840839c5f832dc
greenbel <obisann@gmail.com>
2023-02-08 18:58:21 +0000

    T0-0x15: ensured code doesn't execute on import.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,29 +5,30 @@ information about the TODO list progress of a given employee/user.
 import requests
 from sys import argv
 
-userId = argv[1]
-url_user = 'https://jsonplaceholder.typicode.com/users/{}/'.format(userId)
-url_user_todos = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-        userId)
+if __name__ == '__main__':
+    userId = argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/{}/'.format(userId)
+    url_user_todos = 'https://jsonplaceholder' +\
+        '.typicode.com/users/{}/todos'.format(userId)
 
-# Get data for specified user
-response = requests.get(url_user)
-userName = response.json().get('name')  # retrieve user name
+    # Get data for specified user
+    response = requests.get(url_user)
+    userName = response.json().get('name')  # retrieve user name
 
-# Get data on user's todos
-response = requests.get(url_user_todos)
-jsn = response.json()  # in this case, a list of dictionaries
-totalTodos = len(jsn)
+    # Get data on user's todos
+    response = requests.get(url_user_todos)
+    jsn = response.json()  # in this case, a list of dictionaries
+    totalTodos = len(jsn)
 
-done = 0
-# Get number of tasks completed
-for dct in jsn:
-    if dct.get('completed') is True:
-        done += 1
+    done = 0
+    # Get number of tasks completed
+    for dct in jsn:
+        if dct.get('completed') is True:
+            done += 1
 
-# Present data
-print('Employee {} is done with tasks({}/{}):'.format(
-    userName, done, totalTodos))
-for dct in jsn:
-    if dct.get('completed'):
-        print('\t {}'.format(dct.get('title')))
+    # Present data
+    print('Employee {} is done with tasks({}/{}):'.format(
+        userName, done, totalTodos))
+    for dct in jsn:
+        if dct.get('completed'):
+            print('\t {}'.format(dct.get('title')))
